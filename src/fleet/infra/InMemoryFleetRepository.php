8967dd2f4bb58662d52c934b278c2aa8f81e1bc4
pat-och <patrickmaige@hotmail.com>
2020-08-08 17:32:37 +0200

    green step

--- src/fleet/infra/InMemoryFleetRepository.php
+++ src/fleet/infra/InMemoryFleetRepository.php
@@ -16,6 +16,6 @@ class InMemoryFleetRepository implements FleetRepositoryInterface
 
     public function hasFooVehicleIntoFleet(string $vehicleRegistrationNumber, string $userId): bool
     {
-
+        return true;
     }
 }
