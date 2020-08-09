c17f4968c0602669eca9da8e438af4e899453c5b
pat-och <patrickmaige@hotmail.com>
2020-08-09 09:03:24 +0200

    red step for scenario cantLocalizeMyVehicleToTheSameLocationTwoTimesInARow

--- tests/ParkVehicleCommandHandlerTest.php
+++ tests/ParkVehicleCommandHandlerTest.php
@@ -52,4 +52,47 @@ class ParkVehicleCommandHandlerTest extends TestCase
             $fleetRepository->getfooVehicleGeolocation($fooVehicleRegistrationNumber, $myUserId, $barLocation)
         );
     }
+
+    /**
+     * @test
+     */
+    public function cantLocalizeMyVehicleToTheSameLocationTwoTimesInARow()
+    {
+        /**
+         * arrange
+         */
+
+        // given i
+        $myUserId = 'abc';
+
+        // given i have a fleet
+        $fleetRepository = new InMemoryFleetRepository();
+        $fleetRepository->addFleet($myUserId);
+
+        // given location
+        $barLocation = new Geolocation('43.300000', '5.400000');
+
+        // given a registered vehicle
+        $fooVehicleRegistrationNumber = 'foo';
+        $fleetRepository->addVehicleToFleet($fooVehicleRegistrationNumber, $myUserId, $barLocation);
+
+//        print_r($fleetRepository);die;
+
+        /**
+         * act
+         */
+        $parkVehicleCommandResponse = new ParkVehicleCommandResponse();
+        $parkVehicle = new ParkVehicleCommandHandler($fleetRepository, $parkVehicleCommandResponse);
+        $parkVehicle(
+            new ParkVehicleCommand($myUserId, $fooVehicleRegistrationNumber, $barLocation)
+        );
+
+        /**
+         * assert
+         */
+        $this->assertEquals(
+            'This vehicle is already parked at this location.',
+            $parkVehicleCommandResponse->getError()
+        );
+    }
 }
